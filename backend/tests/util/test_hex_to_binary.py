02c7ec99a34aae5fd2e6c420388925692f730d47
mr4ase <mr4ase@gmail.com>
2026-02-22 08:05:37 +0000

    work on hex to binary functionality

---
+++ backend/tests/util/test_hex_to_binary.py
@@ -0,0 +1,15 @@
+from backend.util.hex_to_binary import hex_to_binary, HEX_TO_BINARY_CONVERSION_TABLE
+from backend.util.crypto_hash import crypto_hash
+
+
+def test_hex_to_binary():
+    test_num = 521
+    hex_test_num = hex(test_num)[2:]
+    binary_test_num = hex_to_binary(hex_test_num)
+
+    assert test_num == int(binary_test_num, 2)
+
+
+def test_num_to_binary_crypto_hash():
+    test_num = 521
+    hex_test_crypto_hash = crypto_hash(str(521))
