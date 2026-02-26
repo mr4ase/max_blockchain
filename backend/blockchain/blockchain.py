cf4c12d8e03279e182165b60ea77389b3f82d08c
mr4ase <mr4ase@gmail.com>
2026-02-26 14:19:52 +0000

    is_valid_blockchain method created

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,10 +1,12 @@
 from backend.blockchain.block import Block
 
+
 class Blockchain:
     """
     Blockchain: a public ledger of transactions.
     Implemented as a list of blocks - data sets of transactions
     """
+
     def __init__(self):
         self.chain = [Block.genesis()]
 
@@ -12,15 +14,33 @@ class Blockchain:
         self.chain.append(Block.mine_block(self.chain[-1], data))
 
     def __repr__(self):
-        return f'Blockchain: {self.chain}'
+        return f"Blockchain: {self.chain}"
+
+    @staticmethod
+    def is_valid_blockchain(chain):
+        """
+        Validate the whole chain.
+        - start with genesis
+        - blocks must be formatted correctly
+
+        """
+        if chain[0] != Block.genesis():
+            raise Exception("The blockchain must start with valid genesis block!")
+
+        for i in range(1, len(chain)):
+            block = chain[i]
+            last_block = chain[i - 1]
+            Block.is_valid_block(last_block=last_block, block=block)
+
 
 def main():
     blockchain = Blockchain()
-    blockchain.add_block('one')
-    blockchain.add_block('two')
+    blockchain.add_block("one")
+    blockchain.add_block("two")
 
     print(blockchain)
-    print(f'blockchain.py __name__: {__name__}')
+    print(f"blockchain.py __name__: {__name__}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
