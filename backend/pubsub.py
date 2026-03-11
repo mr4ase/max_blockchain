43d53aa1ee756a3e510b9b1e814ea267d9117153
mr4ase <mr4ase@gmail.com>
2026-03-11 17:22:14 +0000

    pubsub connection supply

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -2,35 +2,53 @@
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory
 import uuid
+import threading
 
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-11992c54-cdb5-40b8-aebd-6a4c6c312533"
 pnconfig.publish_key = "pub-c-7b59d3ea-64bb-4fd2-a9d3-9220d0e6f60f"
 pnconfig.uuid = str(uuid.uuid4())
-pubnub = PubNub(pnconfig)
 
 TEST_CHANNEL = "TEST_CHANNEL"
+# LISTENER_IS_CONNECTED = threading.Event()
 
 
 class Listener(SubscribeCallback):
+    def __init__(self, connected_event):
+        self.connected_event = connected_event
+
     def message(self, pubnub, message):
-        print(f"\n-- Incoming message: {message}")
+        print(f"\n-- Incoming message: {message.message}")
         # return super().message(pubnub, message)
 
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNConnectedCategory:
+            self.connected_event.set()
 
-pubnub.add_listener(Listener())
-print("Listener added")
 
-print("Subscribing...")
-pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+class PubSub:
+    def __init__(self) -> None:
+        self.connected = threading.Event()
+        self.pubnub = PubNub(pnconfig)
+        listener = Listener(self.connected)
+        self.pubnub.add_listener(listener)
+        self.pubnub.subscribe().channels([TEST_CHANNEL]).execute()
+        self.connected.wait(timeout=5)
+        if not self.connected.is_set():
+            raise RuntimeError("PubNub connection timeout")
+
+    def publish(self, channel: str, message: dict) -> None:
+        self.pubnub.publish().channel(channel).message(message).sync()
 
 
 def main():
+    pubsub = PubSub()
+    message = {"foo": "bar"}
     print("Publishing message...")
-    pubnub.publish().channel(TEST_CHANNEL).message({"foo": "bar"}).sync()
+    pubsub.publish(TEST_CHANNEL, message=message)
     print("Message published...")
-
 
 
 if __name__ == "__main__":
