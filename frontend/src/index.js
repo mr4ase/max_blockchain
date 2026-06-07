1771316ad912a960527ce47ed522d2272dd0116d
mr4ase <mr4ase@gmail.com>
2026-06-07 15:24:00 +0100

    Import frontend from course repository

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import Blockchain from './components/Blockchain';
+import ConductTransaction from './components/ConductTransaction';
+import TransactionPool from './components/TransactionPool';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/blockchain' element={<Blockchain />} />
+      <Route path='/conduct-transaction' element={<ConductTransaction />} />
+      <Route path='/transaction-pool' element={<TransactionPool />} />
+    </Routes>
+  </BrowserRouter>
 );
