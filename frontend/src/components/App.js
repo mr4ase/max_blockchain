24d53fb3abb87ff237be01b6d2f0d11fd3c174cb
mr4ase <mr4ase@gmail.com>
2026-06-07 15:22:09 +0100

    Save current project before importing frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import logo from '../assets/logo.png';
+
+function App() {
+  return (
+    <div className = "App">
+      <img className = "logo" alt="application-logo" src={logo}/>
+      <h3>Welcome to pychain</h3>
+    </div>
+
+  );
+  
+}
+
+export default App;
